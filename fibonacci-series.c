d6afac0c8125aade4b067c063db3f83f96cf50e3
0xshariq <145042629+0xshariq@users.noreply.github.com>
2024-02-06 16:14:01 +0000

    N

---
+++ fibonacci-series.c
@@ -0,0 +1,19 @@
+// C program to print Fibonacci series till nth term using loop
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("enter number ");
+    scanf("%d", &n);
+    
+    int fib[n];
+    fib[0] = 0;
+    fib[1] = 1;
+
+    for(int i = 2; i < n ; i++){
+        fib[i] = fib[i-1] + fib[i-2];
+        printf("%d \t",fib[i]);
+    }
+    
+    return 0;
+}
